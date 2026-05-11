a785af0a62a06dcd8e8109894297aead6bb14935
fanakk328-hub <fanakk328@gmail.com>
2026-05-12 01:42:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from aiogram.filters import Command
 load_dotenv()
 # Убедись, что в файле .env есть строка TOKEN=твой_токен
 TOKEN = os.getenv("TOKEN")
-WEATHER_API_KEY = "7d130988265d37ee60a3e3da9e784cca"
+
 
 # --- ИСПРАВЛЕНО: Создаем объект bot ---
 bot = Bot(token=TOKEN)
